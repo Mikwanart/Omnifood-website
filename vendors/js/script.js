f24bb4695428350fbd48df6b3f5bddef25c0bd72
Michael Nartey <mikwanart7@gmail.com>
2023-10-26 18:04:33 +0000

    green

--- vendors/js/script.js
+++ vendors/js/script.js
@@ -1,5 +1,39 @@
-$(document).ready(function() {
-    $('h1').click(function(){
-        $(this).css('background-color','black')
-    })
-})
+
+$(document).ready(function(){
+
+    /* STICKY Navigation */
+    $('.js--features').waypoint(function(direction){
+        if (direction == "down"){
+            $('nav').addClass('sticky')
+        }
+        else{
+            $('nav').removeClass('sticky')
+        }
+
+       }, {
+        offset: '60px' 
+    });
+
+    /* SCROLL on buttons */
+    $('.js--btnfull').click(function(){
+        $('html, body').animate({scrollTop : $('.js--plans').offset().top}, 1000);
+    });
+
+    $('.js--btnghost').click(function(){
+        $('html, body').animate({scrollTop : $('.js--features').offset().top},1000);
+    });
+
+    /* SCROLL on navigation links */
+        
+
+
+
+
+
+
+
+
+
+});
+
+
